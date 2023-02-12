1dcb6455dc15160956662593b98d7aed60384180
Rex <follienbr1@gmail.com>
2023-02-12 18:43:12 -0500

    add in real server addr instead of localhost 5000

--- client/script.js
+++ client/script.js
@@ -73,7 +73,8 @@ const handleSubmit = async(e) => {
     loader(messageDiv);
 
     // fetch data from server
-    const response = await fetch('http://localhost:5000', {
+    // const response = await fetch('http://localhost:5000', {
+    const response = await fetch( 'https://codex-l2du.onrender.com', {
         method: 'POST',
         headers:{
             'Content-Type': 'application/json'
