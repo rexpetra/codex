f616f78e808fb742d526508e072452da9cc0242f
rexpetra <53532838+rexpetra@users.noreply.github.com>
2023-02-12 18:50:49 -0500

    Update script.js

--- client/script.js
+++ client/script.js
@@ -74,7 +74,7 @@ const handleSubmit = async(e) => {
 
     // fetch data from server
     // const response = await fetch('http://localhost:5000', {
-    const response = await fetch( 'https://codex-l2du.onrender.com', {
+    const response = await fetch( 'https://codex-l2du.onrender.com/', {
         method: 'POST',
         headers:{
             'Content-Type': 'application/json'
